105e262de7ade3553a02df77b39f9d5beb68a3d4
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-09-04 11:46:02 -0400

    commit tree 05d98f146bf2d718bcdf60b2ed5a9e5124d3c57e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    const leftNUmbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNUmbers);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+   if(e.key = 'ArrowLeft'){
+    moveDodgerLeft();
+   } 
+})
+
+function moveDodgerRight(){
+    const leftNUmbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNUmbers);
+
+    if(left < 400){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+})
